9e2875ff7ba42e064bac11f234b1df4f4c83688f
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2024-01-16 22:26:27 +0100

    Add global component navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./pages/Home";
+import {Navbar} from "./components/Navbar";
 
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
+				<Navbar/>
 				<Routes>
 					<Route path="/" element={<Home/>}/>
 				</Routes>
