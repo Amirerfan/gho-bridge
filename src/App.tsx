b12e1d5d02643c06a87152a6c42c08023e0c7a52
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2024-01-16 22:24:37 +0100

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
 
 
 function App() {
-  return (
-    <div className="App">
-      <p className='font-bold'>Hi</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
