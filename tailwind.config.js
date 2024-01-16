26e49db751fbf0c0efa32b3bf1cd115b55c1f4cb
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2024-01-16 22:54:58 +0100

    Add some tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,css,scss}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#2D3250',
+        'primary-light': '#424769',
+        'primary-light2': '#7077A1',
+        'secondary': '#F6B17A',
+        'black': '#08080c',
+        'white': '#eaeaea',
+      }
+    },
   },
   plugins: [],
 }
